213b6582142f21c19bf88b3c388bac0c00d11c47
sx1989827 <395414574@qq.com>
2014-10-17 21:48:19 +0800

    1

--- code/1.0/Core/Core/util/Constant.h
+++ code/1.0/Core/Core/util/Constant.h
@@ -9,7 +9,7 @@
 #ifndef __Core__Constant__
 #define __Core__Constant__
 #ifdef XCODE
-#define SAVEFILE  "/Users/sunxin/me/teambition/code/1.0/Core/data.txt"
+#define SAVEFILE  "data.txt"
 #elif defined(VS)
 #define SAVEFILE "data.txt"
 #endif
