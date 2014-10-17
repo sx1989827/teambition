2c91e07d3d9d338e6aa5ba39a558ff6a10d474d2
sx1989827 <395414574@qq.com>
2014-10-17 22:54:18 +0800

    1

--- code/1.0/Core/Core/main.cpp
+++ code/1.0/Core/Core/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  Core
 //
-//  Created by 孙昕 on 14-10-13.
+//  Created by  on 14-10-13.
 //
 //
 
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[])
     CoreApplication app;
     sNotify noti;
     noti.sec=1000;
-    strcpy(noti.szText,"第三方 drrer");
+    strcpy(noti.szText,"你你 drrer");
     noti.type=sNotify::WORKIOI;
     noti.bEnabled=true;
     noti.flag=222;
