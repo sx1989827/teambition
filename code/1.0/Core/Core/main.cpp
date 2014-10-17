1ebb7bcf145301cf9c968072cf684e93ed007e44
sx1989827 <395414574@qq.com>
2014-10-17 22:31:25 +0800

    1

--- code/1.0/Core/Core/main.cpp
+++ code/1.0/Core/Core/main.cpp
@@ -13,13 +13,14 @@ int main(int argc, const char * argv[])
     CoreApplication app;
     sNotify noti;
     noti.sec=1000;
-    strcpy(noti.szText,"第三方 drrer");
+    strcpy(noti.szText,"的的范德萨发 drrer");
     noti.type=sNotify::WORKIOI;
     noti.bEnabled=true;
     noti.flag=222;
     NOTIFYCENTER->CreateNotify(&noti);
     SAVEINSTANCE->Save();
     SAVEINSTANCE->UnSave();
+	printf("%s", "的冯绍峰的 ");
 }
 
 
