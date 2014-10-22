6c2ec9933ee8d73550a0adedd9939c4d4ab60ef8
sx1989827 <395414574@qq.com>
2014-10-22 10:32:06 +0800

    1

---
+++ code/1.0/Core/Core/Person/PersonBase.cpp
@@ -0,0 +1,9 @@
+//
+//  PersonBase.cpp
+//  Core
+//
+//  Created by 孙昕 on 14/10/21.
+//
+//
+
+#include "PersonBase.h"
---
+++ code/1.0/Core/Core/Person/PersonBase.h
@@ -0,0 +1,17 @@
+//
+//  PersonBase.h
+//  Core
+//
+//  Created by 孙昕 on 14/10/21.
+//
+//
+
+#ifndef __Core__PersonBase__
+#define __Core__PersonBase__
+
+#include "../util/Serializ.h"
+class CorePersonBase:public CoreSerializ
+{
+    
+};
+#endif /* defined(__Core__PersonBase__) */
