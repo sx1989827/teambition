0d961737d29235e321f5fa3702434cd083a7508f
sx1989827 <395414574@qq.com>
2014-12-15 15:09:13 +0800

    1

---
+++ code/1.0/Objc/ForU/ForU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ForU
+//
+//  Created by 孙昕 on 14/12/15.
+//  Copyright (c) 2014年 孙昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
